c647b08a2b26797ea71a9f9f2c8d1f59c7fc864b
shekodn <sdiazn19@gmail.com>
2019-02-28 09:53:07 -0600

    Finishes m2

--- m2/m2.sh
+++ m2/m2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #Changes protagonist
-sed -i 's/Buck Mulligan/Sergio Diaz/g' copy_book.txt
+sed -i.bu 's/Buck Mulligan/Sergio Diaz/g' copy_book.txt
 
 #Chanegs antagonist
-sed -i 's/Haines/Mr. Bean/g' copy_book.txt
+sed -i.bu 's/Haines/Mr. Bean/g' copy_book.txt
 
 # Changes a place
-sed -i 's/Lake/Laka Kamanakaka/g' copy_book.txt
+sed -i.bu 's/Lake/Lake Kamanakaka/g' copy_book.txt
 
 # Changes date and time
-sed -i 's/Monday/Friday/g' copy_book.txt
-sed -i 's/Finn/3/g' copy_book.txt
-sed -i 's/ha/95/g' copy_book.txt
+sed -i.bu 's/Tuesday/FreakyFriday/g' copy_book.txt
+sed -i.bu 's/Fi/F1/g' copy_book.txt
+sed -i.bu 's/lee/133/g' copy_book.txt
