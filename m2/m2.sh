a36a8e94a066691bd64637d776db6a7ee921318f
shekodn <sdiazn19@gmail.com>
2019-02-28 09:26:39 -0600

    Prepare mission2

---
+++ m2/m2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Changes protagonist
+sed -i 's/Buck Mulligan/Sergio Diaz/g' copy_book.txt
+
+#Chanegs antagonist
+sed -i 's/Haines/Mr. Bean/g' copy_book.txt
+
+# Changes a place
+sed -i 's/Lake/Laka Kamanakaka/g' copy_book.txt
+
+# Changes date and time
+sed -i 's/Monday/Friday/g' copy_book.txt
+sed -i 's/Finn/3/g' copy_book.txt
+sed -i 's/ha/95/g' copy_book.txt
